eadfdd99d8b6841d7e2ad59868b7c07256b7a90c
chenzhifeihy <chenzhifeihy@cmhi.chinamobile.com>
2021-03-10 10:28:35 +0800

    feat: linger add duration arg.

--- command/linger.go
+++ command/linger.go
@@ -1,13 +1,19 @@
 package command
 
+import "fmt"
+
 // Linger command
 type Linger struct{
 	Enabled bool
+	Duration int
 }
 
 // BuildMessage Implement command interface
 func (l Linger) BuildMessage() string {
 	if l.Enabled {
+		if l.Duration > 0 {
+			return fmt.Sprintf("linger %d", l.Duration)
+		}
 		return "linger"
 	}
 	return "nolinger"
